e5898830ad1aa8f4f34c551bb2f8a109142b7d1c
Ida <imgjeits@stud.ntnu.no>
2022-04-18 20:40:02 +0200

    intersection observer script to slide/fade in sides and centered elements

---
+++ script.js
@@ -0,0 +1,40 @@
+// Intersection Observer
+const sections = document.querySelectorAll('section')
+
+const sectionObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      const quotes = entry.target.querySelectorAll('p')
+      const leftSide = entry.target.querySelector('.side--left')
+      const rightSide = entry.target.querySelector('.side--right')
+      const button = entry.target.querySelector('button.polluted')
+
+      if (!entry.isIntersecting) {
+        quotes.forEach((quote) => {
+          quote.classList.remove('show')
+        })
+        return
+      }
+      quotes.forEach((quote) => {
+        quote.classList.toggle('show')
+      })
+
+      if (leftSide && rightSide) {
+        leftSide.classList.add('slide-left')
+        rightSide.classList.add('slide-right')
+      }
+
+      if (button) {
+        button.classList.add('show')
+      }
+    })
+  },
+  {
+    rootMargin: '-10px',
+    treshold: 1.0
+  }
+)
+
+sections.forEach((section) => {
+  sectionObserver.observe(section)
+})
